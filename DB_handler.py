f473dbca6f0e9c05e02be101ebe1a5b66228d782
rara-ran <wronin@naver.com>
2021-06-24 19:39:15 +0900

    second

--- DB_handler.py
+++ DB_handler.py
@@ -1,30 +1,82 @@
 import pyrebase
+from datetime import datetime
+import uuid
 
 class DBModule:
     def __init__(self):
             config = {
-                "apiKey": "AIzaSyA0-7CSDV3PzLY4f_N9KUqzt_bTyTjucNM",
-                "authDomain": "flask-firebase-af2d5.firebaseapp.com",
-                "databaseURL": "https://flask-firebase-af2d5-default-rtdb.firebaseio.com",
-                "projectId": "flask-firebase-af2d5",
-                "storageBucket": "flask-firebase-af2d5.appspot.com",
-                "messagingSenderId": "289195730134",
-                "appId": "1:289195730134:web:7d7c8554466bbe4d1c5577",
-                "measurementId": "G-PEV2BE6H79"
+                "apiKey": "AIzaSyDQjLyOXsUE7hv7pQFFWi9pqCudNEh1TK0",
+                "authDomain": "community-data-abb0b.firebaseapp.com",
+                "databaseURL": "https://community-data-abb0b-default-rtdb.firebaseio.com",
+                "projectId": "community-data-abb0b",
+                "storageBucket": "community-data-abb0b.appspot.com",
+                "messagingSenderId": "982750895775",
+                "appId": "1:982750895775:web:d4ddb22fed7c7375bdccaa",
+                "measurementId": "G-S1ET90QQX3"
                 }
             firebase = pyrebase.initialize_app(config)
             self.db = firebase.database()
 
-    def register(self,_id_,pw):
-        self.db.child('user_info').child(_id_).set(pw)
-
-    def login(self,_id,pw):
-        user = self.db.child('user_info').child(_id).get().val()
-        try:
-            if user == pw:
-                print(user)
-                return True
-            else:
-                return False
-        except:
+    #회원가입        
+    def join(self,_id_,pw,nickname,university):
+        if not _id_ or not pw or not nickname:
             return False
+        else:
+            info = {
+                "password": pw,
+                "nickname": nickname,
+                "university": university
+            }
+            self.db.child('user_info').child(_id_).set(info)
+            self.db.child('all_nickname').child(nickname).set({"ID":_id_})
+            return True
+         
+    #아이디 중복 체크 (해당 아이디에 대한 하위값이 있으면 중복True)     
+    def IDcheck(self,_id_):
+        ID = self.db.child('user_info').child(_id_).get().val()
+        if ID != None:
+            return True
+
+    #닉네임 중복 체크 (해당 닉네임에 대한 하위값이 있으면 중복True)
+    def nickname_check(self,nickname):
+        NICKNAME = self.db.child('all_nickname').child(nickname).get().val()
+        print(NICKNAME)
+        if NICKNAME != None:
+            return True  
+    
+    #로그인
+    def login(self,_id_,pw):
+        PASSWORD = self.db.child('user_info').child(_id_).get().val()
+        if PASSWORD !=None:
+            user = PASSWORD['password']
+            try:
+                if user == pw:
+                    return True
+                else:
+                    return False
+            except:
+                return False
+
+    #닉네임 가져오기
+    def nickname_get(self,_id_):
+        nickname_get = self.db.child('user_info').child(_id_).get().val()
+        if nickname_get !=None:
+            nick_get = nickname_get['nickname']
+            return nick_get
+
+    #글쓰기
+    def write(self,nickname,title,contents):
+        if not title or not contents:
+            return False
+        else:
+            data = {
+                "Contents":contents,
+                "Date":datetime.today().strftime("%Y/%m/%d %H:%M:%S")
+                }
+            self.db.child('community').child(nickname).child(title).set(data)
+            return True
+
+    def post_list(self):
+        post_list = self.db.child('community').get().val()
+        print(post_list)
+        
--- app.py
+++ app.py
@@ -1,50 +1,63 @@
-from flask import Flask, redirect,render_template,url_for,request,session, flash
-from DB_handler import DBModule
+from flask import Flask, redirect,render_template,url_for,request,session
+from DB_handler import DBModule;
 
 DB = DBModule()
 app = Flask(__name__)
-app.secret_key = 'WAP_003'
+app.secret_key = 'WAP_003789632145'
 
 @app.route('/',methods=["GET", "POST"])
 def main():
+    if 'login' in session:
+        id =session['login']
+        return render_template('main.html', name = id)
     return render_template('main.html')
 
-@app.route('/home')
-def home():
-    id =session['login_S']
-    return render_template('home.html', name = id)
 
-@app.route('/register', methods=['GET', 'POSt'])
-def register():
+@app.route('/join', methods=['GET', 'POSt'])
+def join():
     if request.method == 'POST':
-        id = request.form['regi_id']
-        pw = request.form['regi_pw']
-        if not id or not pw:
-            return redirect(url_for('register'))
+        id = request.form['id']
+        pw = request.form['password']
+        nickname = request.form['nickname']
+        university = request.form.get('select1')
+        
+        # 아이디 & 닉네임 중복 방지
+        if DB.IDcheck(id) or DB.nickname_check(nickname):
+            return redirect(url_for('join'))
         else:
-            if DB.register(id,pw):
-                return redirect(url_for('register'))
+            if DB.join(id,pw,nickname,university):
+                return redirect(url_for('login'))
             else:
-                return redirect(url_for('main'))
+                return render_template('join.html')
 
-    return render_template('register.html')
+    return render_template('join.html')
+
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        id = request.form['username']
-        pw = request.form['password']
-        if not id or not pw:
-           return redirect(url_for('login'))
-        else: 
-            if DB.login(id, pw):
-                session['login_S'] = id
-                return redirect(url_for('home'))
-            else:
-                flash("아이디가 없거나 비밀번호가 틀립니다.")
-                return redirect(url_for('login'))
+        id = request.form['id']
+        pw = request.form['pw']
+        
+        if DB.login(id, pw):
+            session['login'] = id
+            session['nickname'] = DB.nickname_get(id)
+            return redirect(url_for('main'))
+        else:
+            return redirect(url_for('login'))
 
     return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+    session.pop("login", None)
+    return redirect(url_for("main"))
+
+# @app.route("/post_list")
+# def post_list():
+#     post_list = DB.post_list()
+
+#     return render_template("post_list.html", post_list = post_list)
 
 if __name__ =='__main__':
     app.run(debug=True)
