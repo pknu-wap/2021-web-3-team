b3d1ac70020b603495dd01a184af54cdf146ae23
Hong1270 <ghdwldud5@naver.com>
2021-09-01 00:27:20 +0900

    인증 알고리즘 수정

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect,render_template,url_for,request,session
-from flask_mail import * #설치 필요 pip install Flask-Mail
+from flask_mail import Mail, Message #설치 필요 pip install Flask-Mail
 from DB_handler import DBModule;
 from random import *
 
@@ -10,20 +10,22 @@ app.secret_key = 'WAP_003789632145'
 mail = Mail(app)  
 app.config["MAIL_SERVER"]='smtp.gmail.com'  
 app.config["MAIL_PORT"] = 465      
-app.config["MAIL_USERNAME"] = 'username@gmail.com'  
-app.config['MAIL_PASSWORD'] = '*************'  
+app.config["MAIL_USERNAME"] = 'user@gmail.com'  
+app.config['MAIL_PASSWORD'] = '*****'  
 app.config['MAIL_USE_TLS'] = False  
 app.config['MAIL_USE_SSL'] = True  
 mail = Mail(app)  
 otp = randint(000000,999999)
 
+#홈
 @app.route('/',methods=["GET", "POST"])
 def main():
     if 'login' in session:
         id =session['login']
         return render_template('main.html', name = id)
     return render_template('main.html')
-
+    
+#회원가입
 @app.route('/join', methods=['GET', 'POSt'])
 def join():
     if request.method == 'POST':
@@ -31,13 +33,14 @@ def join():
         pw = request.form['password']
         nickname = request.form['nickname']
         university = request.form.get('select1')
+        email = session['email']
 
         DB.nickname_check(nickname)
         # 아이디 중복 방지, 닉네임 중복방지
         if DB.IDcheck(id) or DB.nickname_check(nickname):
             return redirect(url_for('join'))
         else:
-            if DB.join(id,pw,nickname,university):
+            if DB.join(id,pw,nickname,university,email):
                 return redirect(url_for('login'))
             else:
                 return render_template('join.html')
@@ -48,24 +51,19 @@ def join():
 @app.route('/auth', methods=['GET', 'POST'])
 def auth():
     if request.method == 'POST':
-        email = request.form['email']
-        if not email:
+        per_email = request.form['email']
+        if not per_email:
             return render_template('auth.html')
         else:
+            msg = Message('DAON OTP 인증번호 발송',sender = 'daon@gmail.com', recipients = [per_email])  
+            msg.body = str(otp)  
+            mail.send(msg)
+            session['email'] = per_email  
             return render_template('verify.html')
 
     return render_template('auth.html')
 
 #이메일 인증2
-@app.route('/verify', methods=['GET', 'POSt'])
-def verify():
-    email = request.form["email"]   
-    msg = Message('OTP',sender = 'username@gmail.com', recipients = [email])  
-    msg.body = str(otp)  
-    mail.send(msg)  
-    return render_template('verify.html')
-
-#이메일 인증3
 @app.route('/validate',methods=['GET', 'POSt'])   
 def validate():  
     user_otp = request.form['otp']  
@@ -73,6 +71,7 @@ def validate():
         return render_template('join.html')
     return render_template('auth.html')
 
+#로그인
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -88,32 +87,40 @@ def login():
 
     return render_template('login.html')
 
+#로그아웃
 @app.route("/logout")
 def logout():
     session.pop("login", None)
     return redirect(url_for("main"))
 
+#게시판
 @app.route('/post',methods=["GET", "POST"])
 def post():
     if 'login' in session:
-        return render_template('post.html')
+        post_list = DB.post_list()
+        return render_template('post.html', post_list=post_list.items())
     else:
         return render_template('login.html')
 
+# 게시물 내용 보기
+#@app.route('post/<string:nickname><string:title>')
+#def post_detail(nickname, title):
+    pass
+
 @app.route('/write_page',methods=["GET", "POST"])
 def write_page():
-    return render_template('write.html')
+    return render_template('writing.html')
     
 @app.route('/write',methods=["GET", "POST"])
 def write():
     if request.method == 'POST':       
         title=request.form['title']
-        contents=request.form['contents']
+        content=request.form['content']
 
-        if(DB.write(session['nickname'],title,contents)):
+        if(DB.write(session['nickname'],title,content)):
             return redirect(url_for('post'))
         else:
-            return redirect(url_for('write_page'))
+            return redirect(url_for('writing_page'))
 
 @app.route('/mypage',methods=["GET", "POST"])
 def mypage():
